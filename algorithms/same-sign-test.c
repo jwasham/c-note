2a6eb436048848dfd5930100a7c5299e7fbd0d2e
John Washam <john@readyprompt.com>
2016-02-12 19:08:20 -0800

    Fixed typo.

--- algorithms/same-sign-test.c
+++ algorithms/same-sign-test.c
@@ -2,4 +2,4 @@
 
 int x, y;               // input values to compare signs
 
-bool f = ((x ^ y) < 0); // true iff x and y have opposite signs
+bool f = ((x ^ y) < 0); // true if x and y have opposite signs
