8dd1d582173ee45465d5ad2594976e83b18e60e2
John Washam <john@readyprompt.com>
2016-02-16 14:31:59 -0800

    Started work on UTF-8 checker.

---
+++ algorithms/utf-8-check.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 500
+
+int main()
+{
+    int i;
+    char test[MAX_LENGTH];
+
+    for (i = 0; i < MAX_LENGTH - 1 ; i++) {
+        test[i] = ' ';
+    }
+
+    // in progress
+
+    return 0;
+}
