1491c6eaaa0da9132324f04f82551d38e2892802
John Washam <john@readyprompt.com>
2016-02-17 22:13:45 -0800

    Added linear equation solution.

---
+++ algorithms/solve-linear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+float solve_for_y(float a, float b, float c)
+{
+    float Y = Y = -(b + c) / a;
+    return Y;
+}
+
+main()
+{
+    float a, b, c, Y;
+    printf("\nEnter a linear equation in one variable of the form aY + b + c = 0 ");
+    printf("\nEnter the value of a, b, c respectively: ");
+    scanf("%f%f%f", &a, &b, &c);
+    Y = solve_for_y(a, b, c);
+    printf("\nSolution is Y = %f", Y);
+}
