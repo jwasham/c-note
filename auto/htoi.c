ace8dfdc5ef500b1d569bb8e264a3e8eb29b7c8c
John Washam <john@readyprompt.com>
2016-02-18 11:00:27 -0800

    Finished htoi function.

--- auto/htoi.c
+++ auto/htoi.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-#include <math.h>
 
 int htoi(char hex[])
 {
     int c;
-    int n = 0;
     int v;
+    int n = 0;
 
     for (int i = 0; hex[i] != '\0'; i++) {
         c = hex[i];
         if (c >= '0' && c <= '9') {
             v = c - '0';
         } else if (c >= 'a' && c <= 'f') {
-            v = c - 'a' + 9;
+            v = c - 'a' + 10;
         } else if (c >= 'A' && c <= 'F') {
             v = c - 'A' + 10;
+        } else {
+            continue;
         }
-        n = n * i + v;
+
+        n = 16 * n + v;
     }
 
     return n;
@@ -38,6 +40,27 @@ int main()
     printf("Should be 48: %d\n", htoi("30"));
     printf("Should be 255: %d\n", htoi("FF"));
     printf("Should be 2048: %d\n", htoi("800"));
+    printf("Should be 12113: %d\n", htoi("2F51"));
+    printf("Should be 19008: %d\n", htoi("4A40"));
+    printf("Should be 809149: %d\n", htoi("C58BD"));
+
+    // mixed-case
+
+    printf("Should be 12113: %d\n", htoi("2f51"));
+    printf("Should be 809149: %d\n", htoi("c58bD"));
+
+    // oddballs with non-hex characters
+
+    printf("Should be 0: %d\n", htoi(""));
+    printf("Should be 0: %d\n", htoi("^^^^"));
+    printf("Should be 0: %d\n", htoi("     "));
+    printf("Should be 0: %d\n", htoi("!\nYo\t\a"));
+    printf("Should be 0: %d\n", htoi("0HK"));
+    printf("Should be 0: %d\n", htoi("\05"));
+    printf("Should be 6: %d\n", htoi("6HK"));
+    printf("Should be 103: %d\n", htoi("6Z7Z"));
+    printf("Should be 103: %d\n", htoi("-6Z7Z"));
+    printf("Should be 365307: %d\n", htoi("M592FSUB"));
 
     return 0;
 }
