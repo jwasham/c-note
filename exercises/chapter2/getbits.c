71ceba10ed3fbe594874ea6478fb317148be6cc1
John Washam <john@readyprompt.com>
2016-02-20 12:04:00 -0800

    Finished bits to alpha representation. Not super pretty, but it works.

--- exercises/chapter2/getbits.c
+++ exercises/chapter2/getbits.c
@@ -8,18 +8,28 @@ unsigned int getbits(unsigned int x, int p, int n)
 const char* displaybits(unsigned int d)
 {
     int i = 0;
+    int c;
+    char rbits[sizeof(d) + 1];
     static char bits[sizeof(d) + 1];
 
     if (d == 0) {
-        bits[i] = '0';
-        bits[i + 1] = '\0';
+        rbits[i++] = '0';
     } else {
         while (d != 0) {
-            bits[i++] = (d & 1) == 1 ? '1' : '0';
+            rbits[i++] = (d & 1) == 1 ? '1' : '0';
             d >>= 1;
         }
-        bits[i] = '\0';
     }
+
+    // reverse string
+
+    i--;
+
+    for (c = 0; c <= i; c++) {
+        bits[c] = rbits[i - c];
+    }
+
+    bits[c] = '\0';
 
     return bits;
 }
@@ -35,6 +45,11 @@ int main()
     printf("Should be 110: %s\n", displaybits(6));
     printf("Should be 111: %s\n", displaybits(7));
     printf("Should be 1000: %s\n", displaybits(8));
+    printf("Should be 10000: %s\n", displaybits(16));
+    printf("Should be 11111: %s\n", displaybits(16 + 8 + 4 + 2 + 1));
+    printf("Should be 11011: %s\n", displaybits(16 + 8 + 2 + 1));
+    printf("Should be 10011: %s\n", displaybits(16 + 2 + 1));
+    printf("Should be 10010: %s\n", displaybits(16 + 2));
 
     return 0;
 }
