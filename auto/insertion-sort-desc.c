916d62a10a41483daa25f356d8a143bb3ef6e621
John Washam <john@readyprompt.com>
2016-02-21 13:00:51 -0800

    Finished insertion sort desc.

--- auto/insertion-sort-desc.c
+++ auto/insertion-sort-desc.c
@@ -5,7 +5,15 @@ void insertionSortDesc(int array[], int n)
     int i, j, key;
 
     for (i = 0; i < n; i++) {
+        j = i - 1;
+        key = array[i];
 
+        while (j >= 0 && array[j] < key) {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = key;
     }
 }
 
