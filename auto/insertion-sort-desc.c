5eb39dd2feefed98bcfa2668814eb060bf89d715
John Washam <john@readyprompt.com>
2016-02-21 12:57:59 -0800

    Finished insertion sort asc.

--- auto/insertion-sort-desc.c
+++ auto/insertion-sort-desc.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 
+void insertionSortDesc(int array[], int n)
+{
+    int i, j, key;
 
+    for (i = 0; i < n; i++) {
+
+    }
+}
+
+void printArray(int array[], int n)
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
 
 int main (int argc, char *argv[])
 {
+    int list[] = {3, 5, 2, 7, 9, 14, -1, 4, 2, 5, -5, 1, 0, 38};
 
+    int size = sizeof(list) / sizeof(list[0]);
+
+    insertionSortDesc(list, size);
+
+    printArray(list, size);
 
     return 0;
 }
