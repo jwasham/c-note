5c11562ae45882f48fc146c7a01badfca60d062d
John Washam <john@readyprompt.com>
2016-02-25 11:31:20 -0800

    Solving linear equation in one variable. Work in progress.

---
+++ algorithms/linear-one-variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+float solve_for_y(float a, float b, float c)
+{
+    float Y = Y = -(b + c) / a;
+    return Y;
+}
+
+int main(int argc, char* argv[])
+{
+    float a, b, c, Y;
+    printf("\nEnter a linear equation in one variable of the form aY + b + c = 0 ");
+    printf("\nEnter the value of a, b, c respectively: ");
+    scanf("%f%f%f", &a, &b, &c);
+    Y = solve_for_y(a, b, c);
+    printf("\nSolution is Y = %f\n", Y);
+
+}
