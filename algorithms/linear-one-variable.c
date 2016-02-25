efc8ea2d72c1953cc31a8fb92dfbb89af78bfabf
John Washam <john@readyprompt.com>
2016-02-25 11:33:46 -0800

    Added extra instructions.

--- algorithms/linear-one-variable.c
+++ algorithms/linear-one-variable.c
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 {
     float a, b, c, Y;
     printf("\nEnter a linear equation in one variable of the form aY + b + c = 0 ");
-    printf("\nEnter the value of a, b, c respectively: ");
+    printf("\nEnter the value of a, b, c respectively with spaces between, (ex: 2 16 8.3): ");
     scanf("%f%f%f", &a, &b, &c);
     Y = solve_for_y(a, b, c);
     printf("\nSolution is Y = %f\n", Y);
