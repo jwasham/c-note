56593d8cb61e77bf4742540c04dc97211edba8df
John Washam <john@readyprompt.com>
2016-02-27 14:41:35 -0800

    Added whitespace.

--- algorithms/generate_rand.c
+++ algorithms/generate_rand.c
@@ -5,8 +5,10 @@
 int main(void)
 {
     int num;
+
     /* Seed number for rand() */
     srand((unsigned int) time(0) + getpid());
+
     printf("\nGenerating a random number using srand and rand function.\n");
     num = rand();
 
