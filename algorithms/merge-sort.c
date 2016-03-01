5589b0dd0b43ea92f47524a5ab188ae153c48348
John Washam <john@readyprompt.com>
2016-03-01 09:43:43 -0800

    Code reformat.

--- algorithms/merge-sort.c
+++ algorithms/merge-sort.c
@@ -2,34 +2,30 @@
 #include<stdio.h>
 
 /* Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[] */
-void merge(int arr[], int l, int m, int r)
-{
+void merge(int arr[], int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
-    int n2 =  r - m;
+    int n2 = r - m;
 
     /* create temp arrays */
     int L[n1], R[n2];
 
     /* Copy data to temp arrays L[] and R[] */
-    for(i = 0; i < n1; i++)
+    for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
-    for(j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
     /* Merge the temp arrays back into arr[l..r]*/
     i = 0;
     j = 0;
     k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
             arr[k] = L[i];
             i++;
         }
-        else
-        {
+        else {
             arr[k] = R[j];
             j++;
         }
@@ -37,16 +33,14 @@ void merge(int arr[], int l, int m, int r)
     }
 
     /* Copy the remaining elements of L[], if there are any */
-    while (i < n1)
-    {
+    while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
     /* Copy the remaining elements of R[], if there are any */
-    while (j < n2)
-    {
+    while (j < n2) {
         arr[k] = R[j];
         j++;
         k++;
@@ -55,13 +49,11 @@ void merge(int arr[], int l, int m, int r)
 
 /* l is for left index and r is right index of the sub-array
   of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
-        int m = l+(r-l)/2; //Same as (l+r)/2, but avoids overflow for large l and h
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2; //Same as (l+r)/2, but avoids overflow for large l and h
         mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
+        mergeSort(arr, m + 1, r);
         merge(arr, l, m, r);
     }
 }
@@ -69,19 +61,17 @@ void mergeSort(int arr[], int l, int r)
 
 /* UITLITY FUNCTIONS */
 /* Function to print an array */
-void printArray(int A[], int size)
-{
+void printArray(int A[], int size) {
     int i;
-    for (i=0; i < size; i++)
+    for (i = 0; i < size; i++)
         printf("%d ", A[i]);
     printf("\n");
 }
 
 /* Driver program to test above functions */
-int main()
-{
+int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
 
     printf("Given array is \n");
     printArray(arr, arr_size);
