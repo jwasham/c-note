ba52b3418621d5a0a52be644ff72c5f9e0a7a008
John Washam <john@readyprompt.com>
2016-04-07 10:18:05 -0700

    Reformatted code.

--- algorithms/dice-monte-carlo.c
+++ algorithms/dice-monte-carlo.c
@@ -7,10 +7,15 @@
 #include <time.h>
 
 void initarray(int d[]);
+
 void printarray(int d[]);
+
 void printstar(int n);
+
 void fancyprint(int d[]);
+
 int roll(void);
+
 int numrolls;
 
 int main(void) {
@@ -18,14 +23,9 @@ int main(void) {
     int d[13]; /* 2-12 hold num of rolls */
     int i;     /* loop variable */
     int starttime = time(NULL); /* clock time */
-    srand((long)starttime);
+    srand((long) starttime);
     printf("Enter # of rolls ---> ");
     scanf("%i", &numrolls); /* need & on var */
-
-
-
-
-
 
     initarray(d);
     printf("Total rolls: %1d\n\n", numrolls);
@@ -50,20 +50,20 @@ void initarray(int d[]) {
 void printarray(int d[]) {
     int i;
     double e[] = {0, 0,
-                  1.0/36.0, 2.0/36.0, 3.0/36.0, 4.0/36.0,
-                  5.0/36.0, 6.0/36.0, 5.0/36.0, 4.0/36.0,
-                  3.0/36.0, 2.0/36.0, 1.0/36.0};
+                  1.0 / 36.0, 2.0 / 36.0, 3.0 / 36.0, 4.0 / 36.0,
+                  5.0 / 36.0, 6.0 / 36.0, 5.0 / 36.0, 4.0 / 36.0,
+                  3.0 / 36.0, 2.0 / 36.0, 1.0 / 36.0};
     printf("Sum  Times   Frequency");
     printf("      Exact    Diff\n\n");
     for (i = 2; i < 13; i++)
         printf("%2d %7d %11.7f %10.7f %8.4f\n",
                i,
                d[i],
-               (double)d[i]/numrolls*100.0,
+               (double) d[i] / numrolls * 100.0,
 
-               e[i]*100.0,
-               ((double)(d[i]) -
-                e[i]*numrolls)/numrolls*100.0);
+               e[i] * 100.0,
+               ((double) (d[i]) -
+                e[i] * numrolls) / numrolls * 100.0);
 }
 
 /* printstar: print n stars */
@@ -81,7 +81,7 @@ void fancyprint(int d[]) {
     for (i = 2; i < 13; i++) {
         printf("Sum:%3d |", i);
 
-        printstar(300*d[i]/numrolls);
+        printstar(300 * d[i] / numrolls);
 
         printf("\n");
     }
@@ -90,6 +90,6 @@ void fancyprint(int d[]) {
 
 /* roll: simulate rolling a die */
 int roll(void) {
-    return (int) (6.0*(rand()/(double)RAND_MAX)
+    return (int) (6.0 * (rand() / (double) RAND_MAX)
                   + 1.0);
 }
