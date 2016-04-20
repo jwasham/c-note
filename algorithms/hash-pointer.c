9c6a306e11e428a7fd1275af939a9496d90c12bd
John Washam <john@readyprompt.com>
2016-04-20 09:30:16 -0700

    Added hash pointer

---
+++ algorithms/hash-pointer.c
@@ -0,0 +1,12 @@
+
+#include <limits.h>
+
+#include "hash-pointer.h"
+
+/* Hash function for a generic pointer */
+
+unsigned int pointer_hash(void *location)
+{
+	return (unsigned int) (unsigned long) location;
+}
+
---
+++ algorithms/hash-pointer.h
@@ -0,0 +1,31 @@
+
+/**
+ * @file hash-pointer.h
+ *
+ * Hash function for a generic (void) pointer.  See @ref pointer_hash.
+ */
+
+#ifndef ALGORITHM_HASH_POINTER_H
+#define ALGORITHM_HASH_POINTER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Generate a hash key for a pointer.  The value pointed at by the pointer
+ * is not used, only the pointer itself.
+ *
+ * @param location        The pointer
+ * @return                A hash key for the pointer.
+ */
+
+unsigned int pointer_hash(void *location);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* #ifndef ALGORITHM_HASH_POINTER_H */
+
+
