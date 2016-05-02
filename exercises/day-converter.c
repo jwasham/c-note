c23162e174f0bbac6931236635c14598fd577ef5
John Washam <john@readyprompt.com>
2016-05-02 11:52:20 -0700

    Added day converter from C book exercise

---
+++ exercises/day-converter.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+
+    return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; yearday > daytab[leap][i]; i++)
+        yearday -= daytab[leap][i];
+    *pday = yearday;
+    *pmonth = i;
+}
+
+int main()
+{
+    int day;
+    int month;
+    int year = 1972;
+    int yearday;
+
+    yearday = day_of_year(year, 3, 10);
+
+    month_day(year, yearday, &month, &day);
+
+    printf("yearday: %d\n", yearday);
+    printf("date: %d/%d/%d\n", month, day, year);
+}
