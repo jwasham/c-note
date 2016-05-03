b1e9264100dfeee538abba86fb9f3556352ae88f
John Washam <john@readyprompt.com>
2016-05-03 13:31:33 -0700

    Added date sanity checks to day converter

--- exercises/chapter5/day-converter.c
+++ exercises/chapter5/day-converter.c
@@ -9,6 +9,11 @@ int day_of_year(int year, int month, int day)
 {
     int i, leap;
 
+    if (month < 1 || month > 12 || year < 0 || year > 9999 || day < 1 || day > 31) {
+        printf("Invalid date\n");
+	return 0; 
+    }
+
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
     for (i = 1; i < month; i++)
         day += daytab[leap][i];
@@ -19,6 +24,11 @@ int day_of_year(int year, int month, int day)
 void month_day(int year, int yearday, int *pmonth, int *pday)
 {
     int i, leap;
+
+    if (yearday < 1 || yearday > 366 || year < 0 || year > 9999) {
+        printf("Invalid date\n");
+        return;
+    }
 
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
     for (i = 1; yearday > daytab[leap][i]; i++)
@@ -31,13 +41,15 @@ int main()
 {
     int day;
     int month;
-    int year = 1972;
+    int year = 19732;
     int yearday;
 
     yearday = day_of_year(year, 3, 10);
 
     month_day(year, yearday, &month, &day);
 
-    printf("yearday: %d\n", yearday);
-    printf("date: %d/%d/%d\n", month, day, year);
+    if (yearday > 0) {
+        printf("yearday: %d\n", yearday);
+        printf("date: %d/%d/%d\n", month, day, year);
+    }
 }
