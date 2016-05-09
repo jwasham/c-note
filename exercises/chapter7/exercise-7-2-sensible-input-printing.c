8cf86be45748a5f690776efd12e1af0813ce0473
John Washam <john@readyprompt.com>
2016-05-09 11:04:08 -0700

    Completed exercise 7.2

--- exercises/chapter7/exercise-7-2-sensible-input-printing.c
+++ exercises/chapter7/exercise-7-2-sensible-input-printing.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int isPrintable(int ch)
+{
+    char *printable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 !\"#%&'()*+,-./:;<=>?[\\]^_{|}~\t\f\v\r\n";
+    int found = 0;
+    char *p = printable;
 
+    while (found == 0 && *p != '\0') {
+        if (*p++ == ch) {
+            found = 1;
+        }
+    }
+
+    return found;
+}
+
+int main(int argc, char *argv[])
+{
     int c;
+    int lineLength = 80;
+    int charCount = 0;
 
-    while ((c = getchar()) != EOF)
-        putchar(c);
+    while ((c = getchar()) != EOF) {
+        charCount++;
+        if (charCount > lineLength || c == '\n') {
+            printf("\n");
+            charCount = 0;
+        }
+        if (isPrintable(c)) {
+            printf("%c", c);
+        } else {
+            printf("%x", c);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
